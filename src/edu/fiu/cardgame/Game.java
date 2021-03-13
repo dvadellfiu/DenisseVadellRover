acf449c76d01e0548233bb8ebad80abfed7873b5
mcdenisse <mcdenisse@localhost>
2021-03-13 12:03:45 -0500

    Practice - Week 2

---
+++ src/edu/fiu/cardgame/Game.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.fiu.cardgame;
+
+/**
+ * @author mcdenisse
+ *
+ */
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Deck mydeck = new Deck();
+		mydeck.runSelfCheck();
+	}
+
+}
---
+++ src/edu/fiu/cardgame/Hand.java
@@ -0,0 +1,25 @@
+package edu.fiu.cardgame;
+
+import edu.fiu.sysdesign.SelfCheckCapable;
+
+public class Hand implements SelfCheckCapable {
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean selfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean runSelfCheck() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
